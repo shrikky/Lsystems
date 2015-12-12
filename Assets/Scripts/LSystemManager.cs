62e200787b03e56280f348ae535d1db89c1c606d
Lakshminarayanan Vijayaraghavan <lv1018@rit.edu>
2015-12-12 17:15:39 -0500

    Updated

--- Assets/Scripts/LSystemManager.cs
+++ Assets/Scripts/LSystemManager.cs
@@ -15,28 +15,39 @@ public class LSystemManager : MonoBehaviour
 
     public int maxDepth = 4;
     public string axiom = "0";
-
     private State state;
     public Stack<State> states = new Stack<State>();
     public State currentState;
-
-
     private Dictionary<char, string> rules = new Dictionary<char, string>();
     public GameObject preFab;
     public static LSystemManager getInstance;
-    public Vector3 currentPos = new Vector3(0, 0, 0);
-    private bool isUpdated;
     private int count;
-
+    public float angle;
 
     void Awake()
     {
         getInstance = this;
+
+        // Pythogoras tree
+
         rules.Add('1' , "11");
         rules.Add('0', "1[0]0");
+       // angle = 45;
+
+        // SierPinski triangle
+
+        rules.Add('A', "+B-A-B+");
+        rules.Add('B', "-A+B+A-");
+       // angle = 60;
+
+        rules.Add('X', " X+YF+");
+        rules.Add('Y', "-FX−Y");
+
+
         for(int i =0;i<maxDepth;i++)
         axiom = Expand(axiom);
-       StartCoroutine(ApplyRules(axiom));
+
+       StartCoroutine(DragonCurve(axiom));
     }
    
     string Expand(string input)
@@ -52,7 +63,7 @@ public class LSystemManager : MonoBehaviour
         }
         return s.ToString();
     }
-    public IEnumerator ApplyRules(string input)
+    public IEnumerator PythagorasTree(string input)
     {
         GameObject go;
         currentState.obj = GameObject.Find("root");
@@ -71,10 +82,9 @@ public class LSystemManager : MonoBehaviour
                     currentState.obj = go;
                     currentState.position = go.transform.position;
                     count++;
-                    yield return new WaitForSeconds(0.5f);
+                    yield return null;
                     break;
-                case '1':
-                    
+                case '1':              
                     go = Instantiate(preFab) as GameObject;
                     go.transform.SetParent(currentState.obj.transform);
                     go.transform.rotation = Quaternion.Euler(currentState.dir, 0, 0);
@@ -83,32 +93,146 @@ public class LSystemManager : MonoBehaviour
                     currentState.obj = go;
                     currentState.position = go.transform.position;
                     count++;
-                    yield return new WaitForSeconds(0.5f);
+                    yield return null;
                     break;
+              
                 case '[': // Push position and angle, Then rotate by 45
 
                     states.Push(currentState);
-                    currentState.dir -= 45f;
+                    currentState.dir -= angle;
                     currentState.position = currentState.obj.transform.position;
-
-                    isUpdated = true;
-                    yield return new WaitForSeconds(0.5f);
+                    yield return null;
                     break;
                 case ']':
+
                     // Pop position and angle, turn right by 45
                     currentState = states.Pop();
-                    currentState.dir += 45f;
-                    isUpdated = true;
-                    yield return new WaitForSeconds(0.5f);
-                    break;
-
-                default:
+                    currentState.dir += angle;
+                    yield return null;
                     break;
                  
             }
         }
 
-        Debug.Log(input);
+     //   Debug.Log(input);
+        axiom = input;
+    }
+
+    public IEnumerator Sierpinski(string input)
+    {
+        GameObject go;
+        currentState.obj = GameObject.Find("root");
+        currentState.position = currentState.obj.transform.position;
+
+        foreach (char c in input)
+        {
+            switch (c)
+            {
+                case 'A':
+                    go = Instantiate(preFab) as GameObject;
+                    go.transform.SetParent(currentState.obj.transform);
+                    go.transform.rotation = Quaternion.Euler(currentState.dir, 0, 0);
+                    go.transform.position = currentState.position + go.transform.forward * 10f;
+                    go.name = c.ToString() + count.ToString();
+                    currentState.obj = go;
+                    currentState.position = go.transform.position;
+                    count++;
+                    yield return null;
+                    break;
+
+                case 'B':
+
+                    go = Instantiate(preFab) as GameObject;
+                    go.transform.SetParent(currentState.obj.transform);
+                    go.transform.rotation = Quaternion.Euler(currentState.dir, 0, 0);
+                    go.transform.position = currentState.position + go.transform.forward * 10f;
+                    go.name = c.ToString() + count.ToString();
+                    currentState.obj = go;
+                    currentState.position = go.transform.position;
+                    count++;
+                    yield return null;
+                    break;
+                case '+': // Push position and angle, Then rotate by 45
+
+                    states.Push(currentState);
+                    currentState.dir -= angle;
+                    currentState.position = currentState.obj.transform.position;
+                    yield return null;
+                    break;
+                case '-':
+
+                    // Pop position and angle, turn right by 45
+                    states.Push(currentState);
+                    currentState.dir += angle;
+                    currentState.position = currentState.obj.transform.position;
+                    yield return null;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+      //  Debug.Log(input);
+        axiom = input;
+    }
+
+    public IEnumerator DragonCurve(string input)
+    {
+        GameObject go;
+        currentState.obj = GameObject.Find("root");
+        currentState.position = currentState.obj.transform.position;
+
+        foreach (char c in input)
+        {
+            switch (c)
+            {
+                case 'F':
+                    go = Instantiate(preFab) as GameObject;
+                    go.transform.SetParent(currentState.obj.transform);
+                    go.transform.rotation = Quaternion.Euler(currentState.dir, 0, 0);
+                    go.transform.position = currentState.position + go.transform.forward * 10f;
+                    go.name = c.ToString() + count.ToString();
+                    currentState.obj = go;
+                    currentState.position = go.transform.position;
+                    count++;
+                   // yield return null;
+                    break;
+
+                //case 'X':
+
+                //    go = Instantiate(preFab) as GameObject;
+                //    go.transform.SetParent(currentState.obj.transform);
+                //    go.transform.rotation = Quaternion.Euler(currentState.dir, 0, 0);
+                //    go.transform.position = currentState.position + go.transform.forward * 10f;
+                //    go.name = c.ToString() + count.ToString();
+                //    currentState.obj = go;
+                //    currentState.position = go.transform.position;
+                //    count++;
+                //    yield return null;
+                //    break;
+                case '+': // Push position and angle, Then rotate by 45
+
+                    states.Push(currentState);
+                    currentState.dir -= angle;
+                    currentState.position = currentState.obj.transform.position;
+                   // yield return null;
+                    break;
+                case '-':
+
+                    // Pop position and angle, turn right by 45
+                    states.Push(currentState);
+                    currentState.dir += angle;
+                    currentState.position = currentState.obj.transform.position;
+                    yield return null;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+       // Debug.Log(input);
         axiom = input;
     }
 }
